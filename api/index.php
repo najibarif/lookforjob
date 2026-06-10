dd89e975268ea81aab67e27e4b01c41b87214005
najibarif <nnajiba1357@gmail.com>
2026-06-10 08:38:44 +0700

    Fix Vercel deployment cache issues and suppress deprecations

--- api/index.php
+++ api/index.php
@@ -3,9 +3,18 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Suppress deprecation warnings on Vercel
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes-v7.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
 
 // Vercel serverless environment uses read-only filesystem except /tmp
 $storagePath = '/tmp/storage';
